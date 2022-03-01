10de369ce7e4fcf919d7d58b3c5df57f9336de99
szabados.krisztian@stud.uni-obuda.hu <szabados.krisztian@stud.uni-obuda.hu>
2022-03-01 12:26:50 +0100

    Model superhero implemented

--- SuperHeroes/Models/Superhero.cs
+++ SuperHeroes/Models/Superhero.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace SuperHeroes.Models
 {
+    public enum Side
+    {
+        Good,
+        Evil,
+        Natural
+    }
     class Superhero
     {
+        private int name;
+
+        public int Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int strength;
+
+        public int Strength
+        {
+            get { return strength; }
+            set { strength = value; }
+        }
+
+        private int speed;
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        private Side side;
+
+        public Side Side
+        {
+            get { return side; }
+            set { side = value; }
+        }
+
+        public Superhero Copy()
+        {
+            return new Superhero { Name = this.Name, Strength = this.Strength, Speed = this.Speed, Side = this.Side };
+        }
+
     }
 }
