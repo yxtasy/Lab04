d5b9abc69dfcd93bed3577a3c06dc2afef28ffee
yxtasy <baloghbence123@gmail.com>
2022-03-01 13:02:40 +0100

    Életem munkája, Logic kész

--- SuperHeroes/Logic/Logic.cs
+++ SuperHeroes/Logic/Logic.cs
@@ -12,12 +12,47 @@ namespace SuperHeroes.Logic
     class Logic
     {
         IList<Superhero> superheroes;
-        IList<Superhero> fighter;
+        IList<Superhero> fighters;
         IMessenger messenger;
         ISuperheroEditorService editorService;
 
-        public 
-        
-        
+        public Logic(IMessenger messenger,ISuperheroEditorService editorService)
+        {
+            this.messenger = messenger;
+            this.editorService = editorService;
+        }
+        public double AVGpwr {
+            get
+             {
+                return Math.Round(fighters.Count == 0 ? 0 : fighters.Average(t => t.Strength),2);
+             } 
+            }
+        public double AVGspeed
+        {
+            get
+            {
+                return Math.Round(fighters.Count == 0 ? 0 : fighters.Average(t => t.Speed), 2);
+            }
+        }
+        public void SetupCollections(IList<Superhero> superheroes,IList<Superhero> fighters)
+        {
+            this.superheroes = superheroes;
+            this.fighters = fighters;
+        }
+
+        public void AddToArmy(Superhero superhero)
+        {
+            fighters.Add(superhero.Copy());
+            messenger.Send("Superhero added", "SuperheroInfo");
+        }
+        public void RemoveFromArmy(Superhero superhero)
+        {
+            fighters.Remove(superhero);
+            messenger.Send("Superhero removed", "SuperheroInfo");
+        }
+        public void EditSuperhero(Superhero superhero)
+        {
+            editorService.Edit(superhero);
+        }
     }
 }
