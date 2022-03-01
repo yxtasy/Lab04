ffdc3d755c827cf5dd8040eb056e940111bd89a3
yxtasy <baloghbence123@gmail.com>
2022-03-01 12:49:11 +0100

    W4 SuperheroEditorWindow

---
+++ SuperHeroes/Services/ISuperheroEditorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperHeroes.Models;
+
+namespace SuperHeroes.Services
+{
+    interface ISuperheroEditorService
+    {
+        void Edit(Superhero superhero);
+    }
+}
---
+++ SuperHeroes/Services/SuperheroEditorViaWindow.cs
@@ -0,0 +1,17 @@
+﻿using SuperHeroes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroes.Services
+{
+    class SuperheroEditorViaWindow : ISuperheroEditorService
+    {
+        public void Edit(Superhero superhero)
+        {
+            
+        }
+    }
+}
